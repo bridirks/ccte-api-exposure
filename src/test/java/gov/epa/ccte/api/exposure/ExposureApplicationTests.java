89d69c3ff320542113c3e5c1c0132b27bba37ab9
asif rashid <rashid.asif@epa.gov>
2024-05-22 17:20:55 -0400

    setup tests for container testing. # 38

--- src/test/java/gov/epa/ccte/api/exposure/ExposureApplicationTests.java
+++ src/test/java/gov/epa/ccte/api/exposure/ExposureApplicationTests.java
@@ -1,10 +1,22 @@
 package gov.epa.ccte.api.exposure;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class ExposureApplicationTests {
+	@Container
+	@ServiceConnection
+	static PostgreSQLContainer<?> pgsqldb = new PostgreSQLContainer<>("postgres:13-alpine");
 
 	@Test
 	void contextLoads() {
