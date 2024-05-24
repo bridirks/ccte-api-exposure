45c40adb6184e706a838294375043d8a50782115
bedirks <Dirks.Brianna@epa.gov>
2024-05-24 11:27:08 -0400

    Implemented data layer unit test for ListPresenceTagRepository #52

--- src/test/java/gov/epa/ccte/api/exposure/repository/ListPresenceTagRepositoryTest.java
+++ src/test/java/gov/epa/ccte/api/exposure/repository/ListPresenceTagRepositoryTest.java
@@ -15,7 +15,6 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 import javax.sql.DataSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @Testcontainers
 @DataJpaTest
