9dab7e4073e53c185278e426ee4ddd92417e513f
bedirks <Dirks.Brianna@epa.gov>
2024-05-29 17:49:43 -0400

    Implement data layer unit tests for HttkDataRepository #62

--- src/main/java/gov/epa/ccte/api/exposure/repository/HttkDataRepository.java
+++ src/main/java/gov/epa/ccte/api/exposure/repository/HttkDataRepository.java
@@ -4,8 +4,10 @@ import gov.epa.ccte.api.exposure.domain.HttkData;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 @SuppressWarnings("unused")
 @RepositoryRestResource(exported = false)
 public interface HttkDataRepository extends JpaRepository<HttkData, Integer> {
-    HttkData findByDtxsid(String dtxsid);
+    List<HttkData> findByDtxsid(String dtxsid);
 }
