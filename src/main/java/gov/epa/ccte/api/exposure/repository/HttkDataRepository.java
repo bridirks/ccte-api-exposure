57cea47206044372ec8b9b56dba32f9eecc9e1f1
bedirks <Dirks.Brianna@epa.gov>
2024-05-29 13:30:49 -0400

    Create Httk Data endpoint #37

--- src/main/java/gov/epa/ccte/api/exposure/repository/HttkDataRepository.java
+++ src/main/java/gov/epa/ccte/api/exposure/repository/HttkDataRepository.java
@@ -1,6 +1,5 @@
 package gov.epa.ccte.api.exposure.repository;
 
-import gov.epa.ccte.api.exposure.domain.GenExpoPrediction;
 import gov.epa.ccte.api.exposure.domain.HttkData;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
