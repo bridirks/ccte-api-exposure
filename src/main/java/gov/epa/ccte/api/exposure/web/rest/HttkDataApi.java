4dc15e2a178ac3bedfd76de6520040037e17b422
asif rashid <rashid.asif@epa.gov>
2024-06-18 12:11:59 -0400

    Created Api interface. #72

--- src/main/java/gov/epa/ccte/api/exposure/web/rest/HttkDataApi.java
+++ src/main/java/gov/epa/ccte/api/exposure/web/rest/HttkDataApi.java
@@ -9,12 +9,10 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
--- src/main/java/gov/epa/ccte/api/exposure/web/rest/HttkDataResource.java
+++ src/main/java/gov/epa/ccte/api/exposure/web/rest/HttkDataResource.java
@@ -2,14 +2,7 @@ package gov.epa.ccte.api.exposure.web.rest;
 
 import gov.epa.ccte.api.exposure.domain.HttkData;
 import gov.epa.ccte.api.exposure.repository.HttkDataRepository;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
