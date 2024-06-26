8b25cfa08974606749c1e4e8397cf568bcaae91c
bedirks <Dirks.Brianna@epa.gov>
2024-06-26 16:16:34 -0400

    extract interface general exposure prediction resource #67

--- src/main/java/gov/epa/ccte/api/exposure/web/rest/GenExpoPredictionResource.java
+++ src/main/java/gov/epa/ccte/api/exposure/web/rest/GenExpoPredictionResource.java
@@ -2,12 +2,7 @@ package gov.epa.ccte.api.exposure.web.rest;
 
 import gov.epa.ccte.api.exposure.domain.GenExpoPrediction;
 import gov.epa.ccte.api.exposure.repository.GenExpoPredictionRepository;
-import io.swagger.v3.oas.annotations.Parameter;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
