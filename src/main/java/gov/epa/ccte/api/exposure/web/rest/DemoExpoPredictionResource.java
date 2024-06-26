ec776c64da1673319d444d63c47bdb22feecbe6c
bedirks <Dirks.Brianna@epa.gov>
2024-06-26 11:20:50 -0400

    extract interface demo expo prediction resource #64

--- src/main/java/gov/epa/ccte/api/exposure/web/rest/DemoExpoPredictionResource.java
+++ src/main/java/gov/epa/ccte/api/exposure/web/rest/DemoExpoPredictionResource.java
@@ -2,37 +2,24 @@ package gov.epa.ccte.api.exposure.web.rest;
 
 import gov.epa.ccte.api.exposure.domain.DemoExpoPrediction;
 import gov.epa.ccte.api.exposure.repository.DemoExpoPredictionRepository;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 
-/**
- * REST controller for getting the {@link gov.epa.ccte.api.exposure.domain.DemoExpoPrediction}s.
- */
-@Tag(name = "Demographic Exposure Prediction Resource",
-        description = "API endpoints for demographic prediction in exposure data.")
-@SecurityRequirement(name = "api_key")
 @Slf4j
 @RestController
-public class DemoExpoPredictionResource {
-    private final DemoExpoPredictionRepository repository;
+public class DemoExpoPredictionResource implements DemoExpoPredictionApi {
 
-    public DemoExpoPredictionResource(DemoExpoPredictionRepository repository) {
-        this.repository = repository;
+    final private DemoExpoPredictionRepository demoExpoPredictionRepository;
+
+    public DemoExpoPredictionResource(DemoExpoPredictionRepository demoExpoPredictionRepository) {
+        this.demoExpoPredictionRepository = demoExpoPredictionRepository;
     }
-    @RequestMapping(value = "/exposure/seem/demographic/search/by-dtxsid/{dtxsid}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    DemoExpoPrediction getDemoExpoPredictionByDtxsid(@Parameter(required = true, description = "DSSTox Substance Identifier", example = "DTXSID0020232") @PathVariable("dtxsid")String dtxsid) {
+    @Override
+    public DemoExpoPrediction getDemoExpoPredictionByDtxsid(String dtxsid) {
         log.debug("demographic exposure prediction for dtxsid = {}", dtxsid);
 
-        return repository.findByDtxsid(dtxsid);
-
+        return demoExpoPredictionRepository.findByDtxsid(dtxsid);
 
     }
 }
