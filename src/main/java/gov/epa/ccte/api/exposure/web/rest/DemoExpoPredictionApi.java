25694ae0395dcea54a3e2e2802bd8251876a2fac
Brianna Dirks <148362578+bridirks@users.noreply.github.com>
2024-06-26 11:28:42 -0400

    extract interface demo expo prediction resource #64 (#74)
    * extract interface demo expo prediction resource #64
    
    * updated and now it working fine. #64
    
    ---------
    
    Co-authored-by: asif rashid <rashid.asif@epa.gov>

---
+++ src/main/java/gov/epa/ccte/api/exposure/web/rest/DemoExpoPredictionApi.java
@@ -0,0 +1,36 @@
+package gov.epa.ccte.api.exposure.web.rest;
+
+import gov.epa.ccte.api.exposure.domain.DemoExpoPrediction;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+/**
+ * REST controller for getting the {@link gov.epa.ccte.api.exposure.domain.DemoExpoPrediction}s.
+ */
+@Tag(name = "Demographic Exposure Prediction Resource",
+        description = "API endpoints for demographic prediction in exposure data.")
+@SecurityRequirement(name = "api_key")
+@RequestMapping( value = "exposure/seem/demographic", produces = MediaType.APPLICATION_JSON_VALUE)
+public interface DemoExpoPredictionApi {
+
+    @Operation(summary = "Find demographic prediction SEEMs data by dtxsid", description = "return demographic prediction SEEMs data for requested dtxsid.", tags = {"exposure","seem","demographic"})
+    @ApiResponses(value= {
+            @ApiResponse(responseCode = "200", description = "OK", content = @Content( mediaType = "application/json",
+            schema = @Schema(oneOf = DemoExpoPrediction.class)))
+    })
+    @RequestMapping(value = "/search/by-dtxsid/{dtxsid}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    List<DemoExpoPrediction> getDemoExpoPredictionByDtxsid(@Parameter(required = true, description = "DSSTox Substance Identifier", example = "DTXSID0020232")
+                                                     @PathVariable("dtxsid") String dtxsid);
+}
