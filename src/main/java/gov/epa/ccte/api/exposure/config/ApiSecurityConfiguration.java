cf17a4ea1f737969db492a169d4ed481962cdfbf
asif rashid <rashid.asif@epa.gov>
2024-07-08 18:48:30 -0400

    update the URL for API security.

--- src/main/java/gov/epa/ccte/api/exposure/config/ApiSecurityConfiguration.java
+++ src/main/java/gov/epa/ccte/api/exposure/config/ApiSecurityConfiguration.java
@@ -44,13 +44,21 @@ public class ApiSecurityConfiguration {
         log.debug("*** start filling approvedOriginStore ***");
 
         ConcurrentHashMap<String, String> approvedOriginStore = new ConcurrentHashMap<>();
+        approvedOriginStore.put("https://localhost:3003", "https://localhost:3003");
+        approvedOriginStore.put("https://localhost:8888", "https://localhost:8888");
         approvedOriginStore.put("http://localhost:3003", "http://localhost:3003");
         approvedOriginStore.put("http://localhost:8888", "http://localhost:8888");
         approvedOriginStore.put("https://ccte-ccd-dev.epa.gov", "https://ccte-ccd-dev.epa.gov");
         approvedOriginStore.put("https://ccte-ccd-stg.epa.gov", "https://ccte-ccd-stg.epa.gov");
         approvedOriginStore.put("https://ccte-ccd-prod.epa.gov", "https://ccte-ccd-prod.epa.gov");
+        approvedOriginStore.put("https://ccte-ccd.epa.gov", "https://ccte-ccd.epa.gov");
         approvedOriginStore.put("https://comptox.epa.gov", "https://comptox.epa.gov");
         approvedOriginStore.put("https://ccte-api-s.app.cloud.gov", "https://ccte-api-s.app.cloud.gov");
+        approvedOriginStore.put("https://v2626umcth886.rtord.epa.gov:8888", "https://v2626umcth886.rtord.epa.gov:8888");
+        approvedOriginStore.put("https://v2626umcth877.rtord.epa.gov:8001", "https://v2626umcth877.rtord.epa.gov:8001");
+        approvedOriginStore.put("https://v2626umcth878.rtord.epa.gov:8001", "https://v2626umcth878.rtord.epa.gov:8001");
+        approvedOriginStore.put("https://comptoxstaging.rtpnc.epa.gov", "https://comptoxstaging.rtpnc.epa.gov");
+        approvedOriginStore.put("https://v2626umcth875.rtord.epa.gov:8001", "https://v2626umcth875.rtord.epa.gov:8001");
 
         log.info("*** {} urls are loaded. *** ", approvedOriginStore.size());
 
