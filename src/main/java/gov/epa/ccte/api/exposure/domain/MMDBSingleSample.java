a069b83ea36f3df481de55c3648ea5aa2a72f2d5
Brianna Dirks <148362578+bridirks@users.noreply.github.com>
2025-02-26 17:20:09 -0500

    91 add mmdb endpoints to exposure microservice (#107)
    * Add MMDB endpoints to Exposure Microservice #91
    
    * Add MMDB endpoints to Exposure Microservice #91
    
    * Add MMDB endpoints to Exposure Microservice #91
    
    * Add MMDB endpoints to Exposure microservice #91

---
+++ src/main/java/gov/epa/ccte/api/exposure/domain/MMDBSingleSample.java
@@ -0,0 +1,197 @@
+package gov.epa.ccte.api.exposure.domain;
+
+import java.time.LocalDate;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Schema(description ="The information here describes harmonized single-sample records associated with studies that report sample-level results in MMDB with their associated source and metadata information.")
+@Entity
+@Table(name = "mv_mmdb_single_sample", schema = "mmdb")
+public class MMDBSingleSample {
+	@Id
+	@Column(name = "harmonized_single_id")
+	@Schema(description ="Unique database ID for this individual table record")
+	private Integer id;
+
+	@Size(max = 120)
+	@Column(name = "full_source_name", length = 120)
+	@Schema(description ="Full name of the data source")
+	private String fullSourceName;
+
+	@Size(max = 200)
+	@Column(name = "reported_chemical_name", length = 200)
+	@Schema(description ="Any reported chemical name reported in the raw data")
+	private String chemicalName;
+
+    @Size(max = 20)
+    @Column(name = "dtxsid", length = 20)
+    @Schema(description ="DSSTox chemical structure ID")
+    private String dtxsid;
+
+	@Size(max = 300)
+	@Column(name = "preferred_name", length = 300)
+	@Schema(description ="Preferred chemical name for this DTXSID")
+	private String preferredName;
+
+    @Size(max = 50)
+    @Column(name = "reported_casrn")
+    @Schema(description = "Any reported CAS number reported in the raw data (any format)")
+    private String CASNumber;
+
+    @Size(max = 20)
+    @Column(name = "casrn")
+    @Schema(description = "CASRN associated with this DTXSID")
+    private String casrn;
+
+    @Size(max = 100)
+    @Column(name = "reported_result")
+    @Schema(description = "Any reported concentration or sample result included in the raw data (any format)")
+    private String result;
+
+    @Size(max = 100)
+    @Column(name = "reported_units")
+    @Schema(description = "Any reported units included in the raw data (any format)")
+    private String units;
+
+    @Column(name = "cleaned_units")
+    @Schema(description = "Units from the raw data formatted for consistency")
+    private String cleanedUnits;
+
+    @Size(max = 45)
+    @Column(name = "lod")
+    @Schema(description = "Limit of detection associated witht he study, if available")
+    private String lod;
+
+    @Size(max = 45)
+    @Column(name = "loq")
+    @Schema(description = "Limit of quantification associated witht he study, if available")
+    private String loq;
+
+    @Size(max = 100)
+    @Column(name = "nd_flag")
+    @Schema(description = "Flag indicating if raw data indicated a measurement below limit of detection")
+    private String detectionFlag;
+
+    @Size(max = 500)
+    @Column(name = "result_flag")
+    @Schema(description = "Any reported flags associated with the reported concentrationresult in the raw data")
+    private String resultFlag;
+
+	@Column(name = "detected")
+	@Schema(description ="Single variable indicating whether observation is associated with detection of the chemical (1:yes; 2:no; NA:could not be determined conclusively)")
+	private Integer detected;
+
+	@Column(name = "detect_conflict")
+	@Schema(description ="A quality flag to note if fields used to determine if a chemical was detected were in conflict, resulting in less certain detected field designation (1:the detected field for a chemical sample is less certain; 0:no conflict)")
+	private Integer detectedConflict;
+
+    @Column(name = "detect_note")
+    @Schema(description = "Description of detected status")
+    private String notesDetects;
+
+    @Size(max = 200)
+    @Column(name = "reported_species")
+    @Schema(description = "Any species name (biota) reported in the raw data")
+    private String species;
+
+    @Size(max = 120)
+    @Column(name = "reported_media")
+    @Schema(description = "Any media name reported in the raw data")
+    private String media;
+
+    @Size(max = 200)
+    @Column(name = "harmonized_medium")
+    @Schema(description = "Harmonized medium associated with the reported_media")
+    private String harmonizedMedium;
+
+    @Size(max = 1000)
+    @Column(name = "reported_analytical_method")
+    @Schema(description = "Any reported analytical method information included in the raw data (any format)")
+    private String method;
+
+    @Size(max = 100)
+    @Column(name = "reported_collection_activity_id")
+    @Schema(description = "Any reported collection activity ID reported in the raw data")
+    private String collectionActivityId;
+
+    @Size(max = 100)
+    @Column(name = "reported_sample_id")
+    @Schema(description = "Any reported sample ID reported in the raw data")
+    private String sampleId;
+
+    @Column(name = "mmdb_sample_id")
+    @Schema(description = "Standardized sample ID issued for unique combinations of sample and source for single-sample data")
+    private String mmdbSampleId;
+
+    @Size(max = 45)
+    @Column(name = "reported_dates")
+    @Schema(description = "Any reported date of sample collection in the raw data (any format)")
+    private String dates;
+
+    @Size(max = 4)
+    @Column(name = "sample_year")
+    @Schema(description = "Year of sample collection")
+    private String year;
+
+    @Size(max = 4)
+    @Column(name = "sample_month")
+    @Schema(description = "Month of sample collection (any format)")
+    private String month;
+
+    @Column(name = "sample_time")
+    @Schema(description = "Time of sample collection (any format)")
+    private String time;
+
+    @Size(max = 250)
+    @Column(name = "reported_location")
+    @Schema(description = "Any specific location reported in the raw data that does not fit other location variables (any format)")
+    private String location;
+
+    @Column(name = "state_or_province")
+    @Schema(description = "U.S. state or international province where sample was collected")
+    private String stateOrProvince;
+
+    @Size(max = 60)
+    @Column(name = "us_county")
+    @Schema(description = "U.S. county where sample was collected")
+    private String county;
+
+    @Size(max = 100)
+    @Column(name = "country")
+    @Schema(description = "Country where sample was collected")
+    private String country;
+
+    @Size(max = 100)
+    @Column(name = "qc_flag")
+    @Schema(description = "Flag indicating if raw data included any quality flags for the record")
+    private String qualityFlag;
+
+    @Size(max = 500)
+    @Column(name = "reported_link")
+    @Schema(description = "Any web link to location of the record in the original data source")
+    private String link;
+
+    @Size(max = 300)
+    @Column(name = "reported_reference")
+    @Schema(description = "Any literature reference included in the raw data (any format)")
+    private String reference;
+
+    @Column(name = "data_version", length = Integer.MAX_VALUE)
+    @Schema(description = "Release version of the data")
+    private String version;
+
+    @Column(name = "export_date")
+    @Schema(description = "Date this data was inserted into a materialized view")
+    private LocalDate exportDate;
+}
+
---
+++ src/main/java/gov/epa/ccte/api/exposure/web/rest/MMDBApi.java
@@ -0,0 +1,105 @@
+package gov.epa.ccte.api.exposure.web.rest;
+
+import java.util.List;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import gov.epa.ccte.api.exposure.domain.MMDBAggregate;
+import gov.epa.ccte.api.exposure.domain.MMDBSingleSample;
+import gov.epa.ccte.api.exposure.projection.MMDBMediaDescSummary;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+
+@Tag(name = "MMBD Resource",
+        description = "API endpoints for collecting data for MMDB database.")
+@SecurityRequirement(name = "api_key")
+@RequestMapping( value = "exposure/mmdb")
+public interface MMDBApi {
+
+	/**
+	 * {@code GET  /exposure/mmdb/aggregate/by-dtxsid/:dtxsid} : get list of harmonized aggregate records by dtxsid.
+	 *
+	 * @param dtxsid the matching dtxsid of the MMDB aggregate records to retrieve
+	 * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the list of MMDB aggregate records}.
+	 */
+	@Operation(summary = "Get harmonized aggregate records by dtxsid",
+            description = "Specify the dtxsid as part of the path.")
+    @ApiResponses(value= {
+            @ApiResponse(responseCode = "200", description = "OK",  content = @Content( mediaType = "application/json",
+                    schema=@Schema(oneOf = {MMDBAggregate.class})))
+    })
+    @RequestMapping(value = "/aggregate/by-dtxsid/{dtxsid}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+		List getHarmonizedAggregateDataByDtxsid(@Parameter(required = true, description = "DSSTox Substance Identifier", example = "DTXSID7020182") @PathVariable("dtxsid") String dtxsid);
+
+	/**
+	 * {@code GET  /exposure/mmdb/aggregate/by-medium/:medium} : get list of harmonized aggregate records by harmonized medium.
+	 *
+	 * @param dtxsid the matching dtxsid of the MMDB aggregate records to retrieve
+	 * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the list of MMDB aggregate records}.
+	 */
+	@Operation(summary = "Get harmonized aggregate records by medium",
+            description = "Specify the medium as part of the path.")
+    @ApiResponses(value= {
+            @ApiResponse(responseCode = "200", description = "OK",  content = @Content( mediaType = "application/json",
+                    schema=@Schema(oneOf = {MMDBAggregate.class})))
+    })
+    @RequestMapping(value = "/aggregate/by-medium/{medium}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+		List getHarmonizedAggregateDataByMedium(@Parameter(required = true, description = "harmonized medium", example = "surface water") @PathVariable("medium") String medium);
+
+	/**
+	 * {@code GET  /exposure/mmdb/single-sample/by-dtxsid/:dtxsid} : get list of harmonized single-sample data records by dtxsid.
+	 *
+	 * @param dtxsid the matching dtxsid of the MMDB aggregate records to retrieve
+	 * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the list of MMDB aggregate records}.
+	 */
+	@Operation(summary = "Get harmonized single-sample records by dtxsid",
+            description = "Specify the dtxsid as part of the path.")
+    @ApiResponses(value= {
+            @ApiResponse(responseCode = "200", description = "OK",  content = @Content( mediaType = "application/json",
+                    schema=@Schema(oneOf = {MMDBSingleSample.class})))
+    })
+    @RequestMapping(value = "/single-sample/by-dtxsid/{dtxsid}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+		List getHarmonizedSingleSampleDataByDtxsid(@Parameter(required = true, description = "DSSTox Substance Identifier", example = "DTXSID7020182") @PathVariable("dtxsid") String dtxsid);
+
+
+	/**
+	 * {@code GET  /exposure/mmdb/single-sample/by-medium/:medium} : get list of harmonized single-sample records by dtxsid.
+	 *
+	 * @param dtxsid the matching dtxsid of the MMDB aggregate records to retrieve
+	 * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the list of MMDB aggregate records}.
+	 */
+	@Operation(summary = "Get harmonized single-sample records by medium",
+            description = "Specify the medium as part of the path.")
+    @ApiResponses(value= {
+            @ApiResponse(responseCode = "200", description = "OK",  content = @Content( mediaType = "application/json",
+                    schema=@Schema(oneOf = {MMDBSingleSample.class})))
+    })
+    @RequestMapping(value = "/single-sample/by-medium/{medium}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+		List getHarmonizedSingleSampleDataByMedium(@Parameter(required = true, description = "harmonized medium", example = "surface water") @PathVariable("medium") String medium);
+
+
+	/**
+	 * {@code GET  /exposure/mmdb/mediums} : get list of all searchable medium categories and their definitions.
+	 *
+	 * @param N/A.
+	 * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the list of MMDB medium categories and definitions}.
+	 */
+	@Operation(summary = "Get all searchable harmonized medium categories and their corresponding definitions")
+    @ApiResponses(value= {
+            @ApiResponse(responseCode = "200", description = "OK",  content = @Content( mediaType = "application/json"))
+    })
+    @RequestMapping(value = "/mediums", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	List<MMDBMediaDescSummary> getAllHarmonizedMedia();
+
+}
